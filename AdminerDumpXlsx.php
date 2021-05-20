f2a23568e4b8ac0c5d9dedff7f78e63573ae2a8e
Tomohito Higuchi <tomgoodsun+github@gmail.com>
2021-05-21 01:56:56 +0900

    Change default values of constructor args

--- AdminerDumpXlsx.php
+++ AdminerDumpXlsx.php
@@ -10,21 +10,18 @@ class AdminerDumpXlsx
 {
     private $pathToSheetJs;
     private $pathToFileSaverJs;
-    private $pathtoDumpXlsxJs;
+    private $pathToDumpXlsxJs;
 
     /**
      * @param string $pathToSheetJs
      * @param string $pathToFileSaverJs
      * @param string $pathtoDumpXlsxJs
      */
-    public function __construct(
-        $pathToSheetJs = 'https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.9.10/xlsx.full.min.js',
-        $pathToFileSaverJs = 'https://cdnjs.cloudflare.com/ajax/libs/FileSaver.js/1.3.3/FileSaver.min.js',
-        $pathtoDumpXlsxJs = 'dumpxlsx.js'
-    ) {
-        $this->pathToSheetJs = $pathToSheetJs;
-        $this->pathToFileSaverJs = $pathToFileSaverJs;
-        $this->pathToDumpXlsxJs = $pathtoDumpXlsxJs;
+    public function __construct($pathToSheetJs = null, $pathToFileSaverJs = null, $pathToDumpXlsxJs = null)
+    {
+        $this->pathToSheetJs = $pathToSheetJs ?? 'https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.9.10/xlsx.full.min.js';
+        $this->pathToFileSaverJs = $pathToFileSaverJs ?? 'https://cdnjs.cloudflare.com/ajax/libs/FileSaver.js/1.3.3/FileSaver.min.js';
+        $this->pathToDumpXlsxJs = $pathToDumpXlsxJs ?? 'dumpxlsx.js';
     }
 
     public function head()
